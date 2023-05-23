9957c598c4ffa3d8c56afefdd84bf227c322867a
BT_3W2_12 <mateuszw7@nycstudents.net>
2023-05-23 12:32:23 -0400

    Push 1

--- src/Equations.java
+++ src/Equations.java
@@ -5,7 +5,13 @@ public class Equations {
     private double vf;
     private double a;
 
+    private boolean isT,isD,isVi,isVf,isA;
     public Equations(){
+        isT=false;
+        isD=false;
+        isVi=false;
+        isVf=false;
+        isA=false;
     }
 
     public void setT(double t) {
@@ -25,34 +31,76 @@ public class Equations {
     }
 
     public void solveT(){
-        if(){
+        if(!isD){
             t = (vf-vi)/a;
-        }
-        if(){
+        } else {
             t = d/(0.5*(vf+vi));
         }
-
     }
 
     public void solveD(){
-        if(){
+        if(!isVf){
             d = vi*t + (0.5 * a * Math.pow(t,2));
-        }
-        if(){
+        } else if (!isVi) {
             d = vf*t - (0.5 * a * Math.pow(t,2));
-        }
-        if(){
+        } else if (!isT) {
             d = (Math.pow(vf,2) - Math.pow(vi,2))/(2*a);
-        }
-        if(){
+        } else {
             d = 0.5 * (vf + vi) * t;
         }
     }
 
     public void solveVi(){
-        if(){
-            
+        if(!isD){
+            vi = vf - a * t;
+        } else if (!isA) {
+            vi = 2.0*d/t - vf;
+        } else if (!isT){
+            vi = Math.sqrt(Math.pow(vf,2)-2*a*d);
+        } else {
+            vi = (d-0.5*a*Math.pow(t,2))/t;
         }
     }
 
+    public void solveVf(){
+        if(!isD){
+            vf = vi + a * t;
+        } else if (!isA) {
+            vf = 2.0*d/t - vi;
+        } else if (!isT){
+            vf = Math.sqrt(Math.pow(vf,2)+2*a*d);
+        } else{
+            vf = (d+0.5*a*Math.pow(t,2))/t;
+        }
+    }
+
+    public void solveA() {
+        if (!isT) {
+            a = (Math.pow(vf, 2) - Math.pow(vi, 2)) / (2.0 * d);
+        } else if (!isVi) {
+            a = (d - vf * t) / (-0.5 * Math.pow(t, 2));
+        } else if (!isVf) {
+            a = (d - vf * t) / (0.5 * Math.pow(t, 2));
+        } else {
+            a = (vf - vi) / t;
+        }
+    }
+
+    public void solve(){
+        if(!isD){
+            solveD();
+        }
+        if(!isVi){
+            solveVi();
+        }
+        if(!isVf){
+            solveVf();
+        }
+        if(!isA){
+            solveA();
+        }
+        if(!isT){
+            solveT();
+        }
+    }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class GUI {
+    private JTextField time;
+    private JTextField distance;
+    private JTextField velocityI;
+    private JTextField velocityF;
+    private JTextField acceleration;
+    private JButton solveButton;
+
+    public GUI() {
+
+    }
 }
